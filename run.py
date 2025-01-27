309f7f8f098ce5674714e11b9392a1bcedc10b81
Jarlath Mac S <88r4q7bt@students.codeinstitute.net>
2025-01-27 05:01:12 +0000

    add lives concept to the game, comments throughout

--- run.py
+++ run.py
@@ -18,13 +18,18 @@ def get_valid_word(words):
 def hangman():
 
     hidden_word = get_valid_word(words)
+
+    #this set decreases in length as game is played
     hidden_letters = set(hidden_word)
 
     alphabet_letters = set(string.ascii_uppercase)
-
+    
+    #this set increases in length as game is played
     guessed_letters = set()
 
-    while len(hidden_letters) > 0:
+    lives = 6
+
+    while len(hidden_letters) > 0 and lives > 0:
         sorted_guessed_letters = list(guessed_letters)
         sorted_guessed_letters.sort()
         
@@ -33,21 +38,30 @@ def hangman():
         displayed_characters = [letter if letter in guessed_letters else '-' for letter in hidden_word]
         print('current word is', ' '.join(displayed_characters), '\n')
 
-        guessed_letter = input('guess (enter) a letter: ').upper()
-
+        guessed_letter = input('guess a letter: ').upper()
+        
+        #if user guesses valid letter for first time
         if guessed_letter in (alphabet_letters - guessed_letters):
             guessed_letters.add(guessed_letter)
 
+            #if user guesses letter within the hidden word
             if guessed_letter in hidden_letters:
                 hidden_letters.remove(guessed_letter)
+            else:
+                lives -= 1
+                print(f'you now have {lives} lives')
         
+        #if user guesses valid letter for second time and above
         elif guessed_letter in guessed_letters:
             print(f'you already guessed {guessed_letter}')
 
         else:
             print('invalid character')
 
-    print(f'well done, you solved for {hidden_word}')
+    if lives == 0:
+        print(f'you died, the word was {hidden_word}')
+    else:
+        print(f'well done, you solved for {hidden_word}')
 
 #if __name__ == '__main__':
 hangman()
