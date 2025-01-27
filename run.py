da236f12d06e88aa10d233ebd3c6d8da93c81f09
Jarlath Mac S <88r4q7bt@students.codeinstitute.net>
2025-01-27 01:49:53 +0000

    sort the guessed letters are prompt

--- run.py
+++ run.py
@@ -26,7 +26,7 @@ def hangman():
 
     while len(hidden_letters) > 0:
         
-        print('guessed letters are', ' '.join(guessed_letters)) #need to alphabetize this list
+        print('guessed letters are', ' '.join(list(guessed_letters).sort())) #need to alphabetize this list
 
         displayed_characters = [letter if letter in guessed_letters else '-' for letter in hidden_word]
         print('current word is', ' '.join(displayed_characters), '\n')
