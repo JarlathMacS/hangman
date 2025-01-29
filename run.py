61ae8852141859f37e34d5c2a59443af804de15e
Jarlath Mac S <88r4q7bt@students.codeinstitute.net>
2025-01-29 09:03:14 +0000

    add colorama code and dependency, implemented clear screen functionality more pervasively, fixed capitalisation of prompts

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ import random
 import string
 from words import words
 from simple_term_menu import TerminalMenu
+from colorama import Fore, Back, Style
 
 def get_valid_word(words):
     """chooses a word at random from the list"""
@@ -33,21 +34,25 @@ def hangman():
 
     #hidden letters remain to be guessed, and user still alive
     while len(hidden_letters) > 0 and lives > 0:
+
+        #os.system('cls' if os.name == 'nt' else 'clear')
+
         guessed_letters_list = list(guessed_letters)
         guessed_letters_list.sort()
         
         #only print '6 lives' at start of game
-        if lives == 6 and len(guessed_letters) == 0:
-            print(f'you have {lives} lives')
+        #if lives == 6 and len(guessed_letters) == 0:
+            #os.system('cls' if os.name == 'nt' else 'clear')
+        print(f'You have {lives} lives')
 
         #user has made a guess
         if len(guessed_letters) > 0:
-            print('guessed letters are', ' '.join(guessed_letters_list))
+            print('Guessed letters are', ' '.join(guessed_letters_list))
 
-        displayed_characters = [letter if letter in guessed_letters else '-' for letter in hidden_word]
-        print('word is', ' '.join(displayed_characters), '\n')
+        displayed_characters = [letter if letter in guessed_letters else '_' for letter in hidden_word]
+        print('Word is', ' '.join(displayed_characters), '\n')
 
-        guessed_letter = input('guess a letter: ').upper()
+        guessed_letter = input('Guess a letter: ').upper()
         
         #validation of input conditional statement start
         #if user enters a valid, single letter, for the first time
@@ -56,24 +61,28 @@ def hangman():
 
             #if users' guess is a letter within the hidden word
             if guessed_letter in hidden_letters:
+                os.system('cls' if os.name == 'nt' else 'clear')
                 hidden_letters.remove(guessed_letter)
             else:
                 lives -= 1
-                print(f'no {guessed_letter}, you now have {lives} lives')   #2 e's for correct guesses?
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print(f'No {guessed_letter}')    #, you now have {lives} lives')   #2 e's for correct guesses?
         
         #if user enters a valid, single letter, for the second time or more
         elif guessed_letter in guessed_letters:
-            print(f'you already guessed {guessed_letter}')
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print(f'You already guessed {guessed_letter}')
 
         #if user enters any invalid letter/s, or any other keyboard input/s, at any time
         else:
-            print('invalid character')
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print(f'''{Fore.RED}{Back.WHITE}Invalid character{Fore.WHITE}{Back.BLACK}''')
 
     #user either depleted the hidden letters, or they depleted their lives
     if lives == 0:
-        print(f'you died, the word was {hidden_word}')
+        print(f'You died, the word was {hidden_word}')
     else:
-        print(f'well done, you solved for {hidden_word}')
+        print(f'Well done, you solved for {hidden_word}')
 
 def main():
     """
@@ -83,10 +92,10 @@ def main():
 
     os.system('cls' if os.name == 'nt' else 'clear')
 
-    options = ['Play game', 'Show rules', 'Exit']
-    terminal_menu = TerminalMenu(options)
+    options = ['[1] Play game', '[2] Show rules', '[3] Exit']
+    terminal_menu = TerminalMenu(options, title = 'Options')
     menu_entry_index = terminal_menu.show()
-    print(f'You have selected {options[menu_entry_index]}!')
+    #print(f'You have selected {options[menu_entry_index]}!')
 
     hangman()
 
