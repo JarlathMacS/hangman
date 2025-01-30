d85027bc1cc89614df48863d7f40143eb458b140
Jarlath Mac S <88r4q7bt@students.codeinstitute.net>
2025-01-30 09:03:27 +0000

    add more colours to terminal output, make output read more uniformly across the various possible use cases

--- run.py
+++ run.py
@@ -43,14 +43,14 @@ def hangman():
         #only print '6 lives' at start of game
         #if lives == 6 and len(guessed_letters) == 0:
             #os.system('cls' if os.name == 'nt' else 'clear')
-        print(f'You have {lives} lives')
+        print(f'''{Fore.RED}{Back.WHITE}You have {lives} lives{Style.RESET_ALL}\n\n''')
 
         #user has made a guess
         if len(guessed_letters) > 0:
-            print('Guessed letters are', ' '.join(guessed_letters_list))
+            print(f'''Guessed letters are {Fore.YELLOW}{' '.join(guessed_letters_list)}{Fore.WHITE}\n\n''')
 
         displayed_characters = [letter if letter in guessed_letters else '_' for letter in hidden_word]
-        print('Word is', ' '.join(displayed_characters), '\n')
+        print(f'''Word is {Fore.GREEN}{' '.join(displayed_characters)}{Fore.WHITE}\n\n''')
 
         guessed_letter = input('Guess a letter: ').upper()
         
@@ -66,23 +66,28 @@ def hangman():
             else:
                 lives -= 1
                 os.system('cls' if os.name == 'nt' else 'clear')
-                print(f'No {guessed_letter}')    #, you now have {lives} lives')   #2 e's for correct guesses?
+                print(f'''{Fore.RED}No {guessed_letter}{Style.RESET_ALL}\n\n''')    #, you now have {lives} lives')   #2 e's for correct guesses?
         
         #if user enters a valid, single letter, for the second time or more
         elif guessed_letter in guessed_letters:
             os.system('cls' if os.name == 'nt' else 'clear')
-            print(f'You already guessed {guessed_letter}')
+            print(f'''{Fore.BLUE}{Back.WHITE}You already guessed {guessed_letter}{Style.RESET_ALL}\n\n''')
 
         #if user enters any invalid letter/s, or any other keyboard input/s, at any time
         else:
             os.system('cls' if os.name == 'nt' else 'clear')
-            print(f'''{Fore.RED}{Back.WHITE}Invalid character{Fore.WHITE}{Back.BLACK}''')
+            print(f'''{Fore.RED}Invalid character{Style.RESET_ALL}\n\n''')  #{Fore.WHITE}{Back.BLACK}
 
     #user either depleted the hidden letters, or they depleted their lives
     if lives == 0:
-        print(f'You died, the word was {hidden_word}')
+        #print(f'No {guessed_letter}')
+        print(f'''{Fore.RED}{Back.WHITE}You have {lives} lives, you died{Style.RESET_ALL}\n\n''')
+        print(f'''Guessed letters were {Fore.YELLOW}{' '.join(guessed_letters_list)}{Style.RESET_ALL}\n\n''')
+        print(f'''Word was {Fore.GREEN}{' '.join(displayed_characters)}{Fore.WHITE}\n\n''')
+        print(f'''Solution is {Fore.GREEN}{Back.WHITE}{hidden_word}{Style.RESET_ALL}''')
+        #print(f'You died, the word was {hidden_word}')
     else:
-        print(f'Well done, you solved for {hidden_word}')
+        print(f'''Well done, you solved for {Fore.GREEN}{Back.WHITE}{hidden_word}{Style.RESET_ALL}''')
 
 def main():
     """
