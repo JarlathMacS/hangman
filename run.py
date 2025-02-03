37e845545b3093b861b84fdfa668be9904d0fe09
Jarlath Mac S <88r4q7bt@students.codeinstitute.net>
2025-02-03 11:51:45 +0000

    edit the alignment and positioning of terminal outputs

--- run.py
+++ run.py
@@ -40,13 +40,13 @@ def hangman():
         guessed_letters_list.sort()
         
         print(f'''
-        {Fore.RED}{Back.WHITE}You have {lives} lives remaining{Style.RESET_ALL}
+{Fore.RED}{Back.WHITE}You have {lives} lives remaining{Style.RESET_ALL}
         ''')
 
         #user has made a guess
         if len(guessed_letters) > 0:
             print(f'''
-        You guessed the letters {Fore.YELLOW}{' '.join(guessed_letters_list)}
+You guessed the letters {Fore.YELLOW}{' '.join(guessed_letters_list)}
             {Style.RESET_ALL}
             ''')
 
@@ -54,7 +54,7 @@ def hangman():
         else '_' for letter in hidden_word]
 
         print(f'''
-    The word is {Fore.GREEN}{' '.join(displayed_characters)}{Style.RESET_ALL}
+The word is {Fore.GREEN}{' '.join(displayed_characters)}{Style.RESET_ALL}
         ''')
 
         guessed_letter = input('Guess a letter: ').upper()
@@ -68,7 +68,7 @@ def hangman():
             if guessed_letter in hidden_letters:
                 os.system('cls' if os.name == 'nt' else 'clear')
                 print(f'''
-            {Fore.GREEN}Yes, there's {hidden_word.count(guessed_letter)} letter
+{Fore.GREEN}Yes, there's {hidden_word.count(guessed_letter)} letter
                 {guessed_letter} in the word{Style.RESET_ALL}
                 ''')
                 hidden_letters.remove(guessed_letter)
@@ -76,7 +76,7 @@ def hangman():
                 lives -= 1
                 os.system('cls' if os.name == 'nt' else 'clear')
                 print(f'''
-                {Fore.RED}Sorry, there's no letter {guessed_letter} in the word
+{Fore.RED}Sorry, there's no letter {guessed_letter} in the word
                 {Style.RESET_ALL}\
                 ''')
         
@@ -84,7 +84,7 @@ def hangman():
         elif guessed_letter in guessed_letters:
             os.system('cls' if os.name == 'nt' else 'clear')
             print(f'''
-        {Fore.BLUE}{Back.WHITE}You have already guessed letter {guessed_letter}
+{Fore.BLUE}{Back.WHITE}You have already guessed letter {guessed_letter}
             {Style.RESET_ALL}
             ''')
 
@@ -99,38 +99,38 @@ def hangman():
     #user either depleted the hidden letters, or they depleted their lives
     if lives == 0:
         print(f'''
-    {Fore.RED}{Back.WHITE}Sorry, you had {lives} lives remaining, so you died
+{Fore.RED}{Back.WHITE}Sorry, you had {lives} lives remaining, so you died
         {Style.RESET_ALL}
-        You guessed the letters {Fore.YELLOW}{' '.join(guessed_letters_list)}
+You guessed the letters {Fore.YELLOW}{' '.join(guessed_letters_list)}
         {Style.RESET_ALL}
-    The word was {Fore.GREEN}{' '.join(displayed_characters)}{Style.RESET_ALL}
-        The answer was {Fore.GREEN}{Back.WHITE}{hidden_word}{Style.RESET_ALL}
+The word was {Fore.GREEN}{' '.join(displayed_characters)}{Style.RESET_ALL}
+The answer was {Fore.GREEN}{Back.WHITE}{hidden_word}{Style.RESET_ALL}
         ''')
     else:
         print(f'''
 {Fore.RED}{Back.WHITE}You still had {lives} lives remaining{Style.RESET_ALL}
-        You guessed the letters {Fore.YELLOW}{' '.join(guessed_letters_list)}
+You guessed the letters {Fore.YELLOW}{' '.join(guessed_letters_list)}
         {Style.RESET_ALL}
-        The word was {Fore.GREEN}{' '.join(displayed_characters)}
+The word was {Fore.GREEN}{' '.join(displayed_characters)}
 {Back.WHITE}Well done, you correctly answered {hidden_word}{Style.RESET_ALL}
         ''')
 
 
 def display_rules():
     print(f'''
-    {Fore.YELLOW}[1] {Fore.CYAN}The aim of this classic game is to guess the
-    hidden word before you lose all your lives
-    {Fore.YELLOW}[2] {Fore.CYAN}You get six (6) lives at the start of the game
-    {Fore.YELLOW}[3] {Fore.CYAN}Only the 26 alphabet letters (A to Z) are valid
-    guesses
-    {Fore.YELLOW}[4] {Fore.CYAN}You may only guess a single letter at a time
-    {Fore.YELLOW}[5] {Fore.CYAN}Invalid guesses don't count towards your lives
-    {Fore.YELLOW}[6] {Fore.CYAN}Repeated guesses don't count towards your lives
-    {Fore.YELLOW}[7] {Fore.CYAN}The hidden words are chosen at random by the
-    computer
-    {Fore.YELLOW}[8] {Fore.CYAN}There are no spaces ( ) or hyphens (-) in the
-    hidden words
-    {Fore.YELLOW}[9] {Fore.CYAN}Have fun!{Style.RESET_ALL}
+{Fore.YELLOW}[1] {Fore.CYAN}The aim of this classic game is to guess the
+hidden word before you lose all your lives
+{Fore.YELLOW}[2] {Fore.CYAN}You get six (6) lives at the start of the game
+{Fore.YELLOW}[3] {Fore.CYAN}Only the 26 alphabet letters (A to Z) are valid
+guesses
+{Fore.YELLOW}[4] {Fore.CYAN}You may only guess a single letter at a time
+{Fore.YELLOW}[5] {Fore.CYAN}Invalid guesses don't count towards your lives
+{Fore.YELLOW}[6] {Fore.CYAN}Repeated guesses don't count towards your lives
+{Fore.YELLOW}[7] {Fore.CYAN}The hidden words are chosen at random by the
+computer
+{Fore.YELLOW}[8] {Fore.CYAN}There are no spaces ( ) or hyphens (-) in the
+hidden words
+{Fore.YELLOW}[9] {Fore.CYAN}Have fun!{Style.RESET_ALL}
     ''')
 
 
