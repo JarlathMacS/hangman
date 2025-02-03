977c7f2fec9592c9528f8eb98ae823392e16fb01
Jarlath Mac S <88r4q7bt@students.codeinstitute.net>
2025-02-03 14:37:15 +0000

    further edit the alignment and positioning of terminal outputs, to comply with pep8

--- run.py
+++ run.py
@@ -59,7 +59,7 @@ The word is {Fore.GREEN}{' '.join(displayed_characters)}{Style.RESET_ALL}
 
         guessed_letter = input('Guess a letter: ').upper()
         
-        #validation of input conditional statement start
+        #validation of input, conditional statement start
         #if user enters a valid, single letter, for the first time
         if guessed_letter in (alphabet_letters - guessed_letters):
             guessed_letters.add(guessed_letter)
@@ -68,24 +68,24 @@ The word is {Fore.GREEN}{' '.join(displayed_characters)}{Style.RESET_ALL}
             if guessed_letter in hidden_letters:
                 os.system('cls' if os.name == 'nt' else 'clear')
                 print(f'''
-{Fore.GREEN}Yes, there's {hidden_word.count(guessed_letter)} letter
-                {guessed_letter} in the word{Style.RESET_ALL}
+                {Fore.GREEN}
+Yes, there's {hidden_word.count(guessed_letter)} of letter {guessed_letter} in\
+ the word{Style.RESET_ALL}
                 ''')
                 hidden_letters.remove(guessed_letter)
             else:
                 lives -= 1
                 os.system('cls' if os.name == 'nt' else 'clear')
                 print(f'''
-{Fore.RED}Sorry, there's no letter {guessed_letter} in the word
-                {Style.RESET_ALL}\
+{Fore.RED}Sorry, there's no letter {guessed_letter} in the word\
+{Style.RESET_ALL}
                 ''')
         
         #if user enters a valid, single letter, for the second time or more
         elif guessed_letter in guessed_letters:
             os.system('cls' if os.name == 'nt' else 'clear')
             print(f'''
-{Fore.BLUE}{Back.WHITE}You have already guessed letter {guessed_letter}
-            {Style.RESET_ALL}
+{Fore.BLUE}You have already guessed letter {guessed_letter}{Style.RESET_ALL}
             ''')
 
         #if user enters any invalid letter/s, or any other keyboard input/s, 
@@ -93,31 +93,39 @@ The word is {Fore.GREEN}{' '.join(displayed_characters)}{Style.RESET_ALL}
         else:
             os.system('cls' if os.name == 'nt' else 'clear')
             print(f'''
-{Fore.RED}You have guessed an invalid character.  Only letters A to Z are valid
+{Fore.RED}You have guessed an invalid character.  Only letters A to Z are\
+ valid, and only\none (1) letter at a time
             ''')
 
     #user either depleted the hidden letters, or they depleted their lives
     if lives == 0:
         print(f'''
-{Fore.RED}{Back.WHITE}Sorry, you had {lives} lives remaining, so you died
-        {Style.RESET_ALL}
+{Fore.RED}{Back.WHITE}Sorry, you had {lives} lives remaining, so you died\
+{Style.RESET_ALL}
+
 You guessed the letters {Fore.YELLOW}{' '.join(guessed_letters_list)}
         {Style.RESET_ALL}
 The word was {Fore.GREEN}{' '.join(displayed_characters)}{Style.RESET_ALL}
+
 The answer was {Fore.GREEN}{Back.WHITE}{hidden_word}{Style.RESET_ALL}
         ''')
     else:
         print(f'''
 {Fore.RED}{Back.WHITE}You still had {lives} lives remaining{Style.RESET_ALL}
+
 You guessed the letters {Fore.YELLOW}{' '.join(guessed_letters_list)}
         {Style.RESET_ALL}
 The word was {Fore.GREEN}{' '.join(displayed_characters)}
-{Back.WHITE}Well done, you correctly answered {hidden_word}{Style.RESET_ALL}
+
+Well done, you correctly answered {Back.WHITE}{hidden_word}{Style.RESET_ALL}
         ''')
 
 
 def display_rules():
     print(f'''
+    {Fore.CYAN}
+Hangman Rules:
+
 {Fore.YELLOW}[1] {Fore.CYAN}The aim of this classic game is to guess the
 hidden word before you lose all your lives
 {Fore.YELLOW}[2] {Fore.CYAN}You get six (6) lives at the start of the game
@@ -127,7 +135,7 @@ guesses
 {Fore.YELLOW}[5] {Fore.CYAN}Invalid guesses don't count towards your lives
 {Fore.YELLOW}[6] {Fore.CYAN}Repeated guesses don't count towards your lives
 {Fore.YELLOW}[7] {Fore.CYAN}The hidden words are chosen at random by the
-computer
+program
 {Fore.YELLOW}[8] {Fore.CYAN}There are no spaces ( ) or hyphens (-) in the
 hidden words
 {Fore.YELLOW}[9] {Fore.CYAN}Have fun!{Style.RESET_ALL}
